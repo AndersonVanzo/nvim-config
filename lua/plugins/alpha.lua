a72a18596c6cf5164a1a85dd4b39526cdf8cde10
AndersonVanzo <anderson.vanzo01@gmail.com>
2025-01-31 20:42:26 -0300

    create config

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,49 @@
+return {
+
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
+
+		dashboard.section.buttons.val = {}
+
+		_Gopts = {
+			position = "center",
+			hl = "Type",
+		}
+
+        local function footer()
+            return "programming is an art of patience, not talent"
+        end
+
+        dashboard.section.footer.val = footer()
+
+		dashboard.opts.opts.noautocmd = true
+		alpha.setup(dashboard.opts)
+	end,
+}
