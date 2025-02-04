28dd8cf4116bcc67e68e93560c4cc7222c5b5ff0
AndersonVanzo <anderson.vanzo01@gmail.com>
2025-02-03 22:17:15 -0300

    fix(telescope): ignore node_modules and .dist

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,14 @@ return {
 			"nvim-lua/plenary.nvim",
 		},
 		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"node_modules",
+						".dist",
+					},
+				},
+			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
